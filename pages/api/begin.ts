16a91dde6c1c8b73c61c68dfeffe95fc3ff555fe
Marcel Lasaj <xxx.mazzel.xxx+github@gmail.com>
2020-12-13 18:31:59 +0100

    Added proper configuration for Netlify CMS

---
+++ pages/api/begin.ts
@@ -0,0 +1,4 @@
+// @ts-ignore: This package doesn't have any typings
+import {createNowBeginHandler} from 'netlify-cms-oauth-provider-node';
+
+module.exports = createNowBeginHandler({}, {useEnv: true});
